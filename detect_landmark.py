5c672b9d138776b93a5aa4193f6ea3f5fb385c4f
Arnold Agus <arnoldagus@Arnolds-MacBook-Pro.local>
2017-04-24 09:21:07 +1000

    initial commit yep

---
+++ detect_landmark.py
@@ -0,0 +1,32 @@
+def outputLandmarks(landmarks):
+    for landmark in landmarks:
+        print(landmark.description)
+
+def identifyLandmarkLocal(path):
+    from google.cloud import vision_client
+    visionClient = vision.Client()
+    with io.open(path, 'rb') as imageFile:
+        content = imageFile.read()
+
+    image = visionClient.image(content=content)
+    return image.detect_landmarks
+
+
+def identifyLandmark(cloudUri):
+    from google.cloud import vision_client
+    visionClient = vision.Client()
+
+    image = visionClient.image(source_uri=cloudUri)
+
+    return image.detect_landmarks()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Identifies the landmark in the given image.')
+    parser.add_argument(
+        'gcs_uri', help=('The Google Cloud Storage uri to the image to identify'
+                         ', of the form: gs://bucket_name/object_name.jpg'))
+    args = parser.parse_args()
+
+    outputLandmarks(identifyLandmark(args.gcs_uri)) 
+
